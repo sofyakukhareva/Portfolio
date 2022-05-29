9d0a2383c70888a96f53114c3fdf179a9eb47147
sofyakukhareva <s.kuxareva@gmail.com>
2022-05-30 00:55:15 +0300

    upd:project page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', () => {  
     let scrollTop = document.documentElement.scrollTop;
-    console.log(scrollTop);
     document.getElementById('zoom').style.width = 40 + scrollTop / 5 + '%';
   });
